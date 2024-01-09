f5938c6b529f72d46ee275ab92bb708f53952187
zakifrhn <zaki.farhan026@gmail.com>
2024-01-09 09:35:11 +0700

    penambahan dotenv

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-const express = require('express')
-const app = express()
-const routers = require('./src/routers/index')
-const db =  require('./src/config/db')
-const cors = require('cors')
-const cloudinary = require('cloudinary').v2
+const express = require("express");
+const app = express();
+const routers = require("./src/routers/index");
+const db = require("./src/config/db");
+const cors = require("cors");
+const cloudinary = require("cloudinary").v2;
+require("dotenv").config();
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(routers)
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(routers);
 db.connect()
-.then(() => {
-
+  .then(() => {
     cloudinary.config({
-        cloud_name: process.env.CD_NAME,
-        api_key: process.env.CD_KEY,
-        api_secret: process.env.CD_SECRET
-    })  
+      cloud_name: process.env.CD_NAME,
+      api_key: process.env.CD_KEY,
+      api_secret: process.env.CD_SECRET,
+    });
 
-    app.listen(process.env.PORT,() => {
-        console.log('app running on port 5200');
-    })
-})
-.catch(e => {
-    console.log(`your port ${e} is not recongnize`)
-})
+    app.listen(process.env.PORT, () => {
+      console.log("app running on port 5200");
+    });
+  })
+  .catch((e) => {
+    console.log(`your port ${e} is not recongnize`);
+  });
